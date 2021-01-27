f682ef40d7231eb0fc7ae571fd7c78fba9abc4bc
soo <dge05207@gmail.com>
2021-01-27 13:30:56 +0900

    Ignored files

--- build/object_mapping/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/soo/object_mapping/src/object_mapping
-# Build directory: /home/soo/object_mapping/build/object_mapping
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
