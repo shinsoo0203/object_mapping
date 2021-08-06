2a4a8520ee0dfd22835d310c5c42902db1edc1fb
shinsoo0203 <dge05207@gmail.com>
2021-08-06 21:28:59 +0900

    Add grid map

--- src/object_mapping/src/grid_mapping.cpp
+++ src/object_mapping/src/grid_mapping.cpp
@@ -13,24 +13,68 @@
 
 #include <ros/ros.h>
 #include <iostream>
+#include <grid_map_ros/grid_map_ros.hpp>
+
+using namespace grid_map;
 
 class GridMapping{
 
 private:
+  ros::NodeHandle nh;
+  ros::Publisher grid_mapper;
 
 public:
   GridMapping(){
+    grid_mapper = nh.advertise<grid_map_msgs::GridMap>("grid_map", 1, true);
   }
   ~GridMapping(){}
 
-  void main(){
+  void main()
+  {
+    // Create grid map
+    GridMap map({"elevation", "normal_x", "normal_y", "normal_z"});
+    map.setFrameId("map");
+    map.setGeometry(Length(500, 500), 3.0, Position(0.0, 0.0));
+    ROS_INFO("Created map with size %f x %f m (%i x %i cells).\n The center of the map is located at (%f, %f) in the %s frame.",
+      map.getLength().x(), map.getLength().y(),
+      map.getSize()(0), map.getSize()(1),
+      map.getPosition().x(), map.getPosition().y(), map.getFrameId().c_str());
+
+    ros::Rate rate(30.0);
+    while(nh.ok()){
+      ros::Time time = ros::Time::now();
+
+      // iterating through grid map and adding data
+      for (GridMapIterator it(map); !it.isPastEnd(); ++it) {
+        Position position;
+        map.getPosition(*it, position);
+        map.at("elevation", *it) = 1;
+        Eigen::Vector3d normal(1, 1, 1);
+
+        normal.normalize();
+        map.at("normal_x", *it) = normal.x();
+        map.at("normal_y", *it) = normal.y();
+        map.at("normal_z", *it) = normal.z();
+      }
+
+      // Publish grid map.
+      map.setTimestamp(time.toNSec());
+      grid_map_msgs::GridMap message;
+      GridMapRosConverter::toMessage(map, message);
+      grid_mapper.publish(message);
+      ROS_INFO_THROTTLE(1.0, "Grid map (timestamp %f) published.", message.info.header.stamp.toSec());
+
+      rate.sleep();
+    }
   }
 };
 
 int main(int argc, char** argv)
 {
     ros::init(argc, argv, "grid_mapping");
+
     GridMapping gm;
     gm.main();
+
     return 0;
 }
